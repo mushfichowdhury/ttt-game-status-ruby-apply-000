e36e72055152f5cdec4605595e5c3ba39396adaf
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-06-15 01:12:02 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -61,28 +61,6 @@ def won?(board)
      return WIN_COMBINATIONS[3]
     elsif position_1 == "O" && position_4 == "O" && position_7 == "O"
      return WIN_COMBINATIONS[3]
-     
-      elsif position_1 == "X" && position_2 == "X" && position_3 == "X"
-     return win_combination
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-     return win_combination
-     
-      elsif position_1 == "X" && position_2 == "X" && position_3 == "X"
-     return win_combination
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-     return win_combination
-     
-      elsif position_1 == "X" && position_2 == "X" && position_3 == "X"
-     return win_combination
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-     return win_combination
-     
-      elsif position_1 == "X" && position_2 == "X" && position_3 == "X"
-     return win_combination
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-     return win_combination
-   
-     
     end
     return false
   end

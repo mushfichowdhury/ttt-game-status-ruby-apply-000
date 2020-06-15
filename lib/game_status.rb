19fba00b931a5dc95a7242618d041b7ec98444bd
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-06-15 00:42:25 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,7 +8,7 @@ end
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    
+    index_0 = win_combination[0]
     if win_combination[0] == "X" && win_combination[1] == "X" && win_combination[2] == "X"
      return WIN_COMBINATIONS[0]
     else
